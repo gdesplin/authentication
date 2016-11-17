b3f67ce692f08f88889cc8a2b1f84b70a664390c
gdesplin <gdesplin@gmail.com>
2016-11-16 21:51:25 -0700

    lots of stuff, users

--- goalserver.py
+++ goalserver.py
@@ -3,9 +3,10 @@ from urllib.parse import urlparse, parse_qs
 from datetime import datetime
 import json
 import sqlite3
-from sqlclass import goalsdb
+from sqlclass import goalsdb, usersdb
 
 class MyServer(BaseHTTPRequestHandler):
+#goals
         def createGoal(self):
             sql = goalsdb()
             self.send_response(201)
@@ -13,12 +14,7 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
             length = int(self.headers['Content-Length'])
             data = self.rfile.read(length).decode("utf-8")
-            print("start recieved data")
-            print(data)
-            print('end')
             parsed_data = parse_qs(data, False, True)
-            print('parsed')
-            print(parsed_data)
             sql.sqlCreateGoal(parsed_data)
 
         def getGoals(self):
@@ -35,7 +31,6 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_header('Access-Control-Allow-Origin','*')
             self.end_headers()
             goal = sql.getGoal(key)
-            print(goal)
             self.wfile.write(bytes(str.encode(goal)))
 
         def updateGoal(self, key):
@@ -47,7 +42,6 @@ class MyServer(BaseHTTPRequestHandler):
             data = self.rfile.read(length).decode("utf-8")
             goal = parse_qs(data)
             goallist = sql.dictToList(goal)
-            print(goallist)
             sql.updateGoal(goal, key)
 
         def deleteGoal(self, key):
@@ -55,13 +49,59 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Access-Control-Allow-Origin','*')
             self.end_headers()
-            print(key)
             sql.deleteGoal(key)
 
-        def check_key(self, key):
+        def check_key_goals(self, key):
             sql = goalsdb()
-            return sql.check_key(key)
-            
+            return sql.check_key_goals(key)
+
+        def createUser(self):
+            sql = usersdb()
+            self.send_response(201)
+            self.send_header('Access-Control-Allow-Origin','*')
+            self.end_headers()
+            length = int(self.headers['Content-Length'])
+            data = self.rfile.read(length).decode("utf-8")
+            parsed_data = parse_qs(data, False, True)
+            sql.sqlCreateUser(parsed_data)
+#users
+        def getUsers(self):
+            sql = usersdb()
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin','*')
+            self.end_headers()
+            users = sql.getUsers()
+            self.wfile.write(bytes(str.encode(users)))
+
+        def getUser(self, key):
+            sql = usersdb()
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin','*')
+            self.end_headers()
+            goal = sql.getUser(key)
+            self.wfile.write(bytes(str.encode(goal)))
+
+        def updateUser(self, key):
+            sql = usersdb()
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin','*')
+            self.end_headers()
+            length = int(self.headers['Content-Length'])
+            data = self.rfile.read(length).decode("utf-8")
+            goal = parse_qs(data)
+            goallist = sql.dictToList(goal)
+            sql.updateUser(goal, key)
+
+        def deleteUser(self, key):
+            sql = usersdb()
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin','*')
+            self.end_headers()
+            sql.deleteUser(key)
+        
+        def check_key_users(self, key):
+            sql = usersdb()
+            return sql.check_key_users(key)
 
         def send_404(self):
             self.send_response(404)
@@ -69,24 +109,41 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes("Page not found. 404", "utf-8"))
 
-
+#Do Routes
         def do_GET(self):
             query = parse_qs(urlparse(self.path).query)
             splitq = self.path.split("/")
-            if len(splitq) == 3:
-                key = splitq[2]
-                if self.check_key(key):
-                    self.getGoal(key)
-                else:
-                    self.send_404()
-            elif self.path.startswith("/goals"):
-                self.getGoals()
+            if splitq[1] == "goals":
+                if len(splitq) == 3:
+                    key = splitq[2]
+                    if self.check_key_goals(key):
+                        self.getGoal(key)
+                    else:
+                        self.send_404()
+                elif self.path.startswith("/goals"):
+                    self.getGoals()
+            elif sqlitq[1] == "users":
+                if len(splitq) == 3:
+                    key = splitq[2]
+                    if self.check_key_users(key):
+                        self.getUser(key)
+                    else:
+                        self.send_404()
+                elif len(splitq) == 2:
+                    self.getUsers()
             else:
                 self.send_404()
 
         def do_POST(self):
-            if self.path.startswith("/goals"):
-                self.createGoal()   
+            query = parse_qs(urlparse(self.path).query)
+            splitq = self.path.split("/")
+            if len(splitq) == 2:
+                if splitq[1] == "goals":
+                    self.createGoal()   
+                elif splitq[1] == "users":
+                    self.createUser()
+                else:
+                    self.send_404()
             else:
                 self.send_404()
 
@@ -100,12 +157,20 @@ class MyServer(BaseHTTPRequestHandler):
 
         def do_PUT(self):
             splitq = self.path.split("/")
-            if len(splitq) == 3:
-                key = splitq[2]
-                if self.check_key(key):
-                    self.updateGoal(key)
-                else:
-                    self.send_404()
+            if splitq[1] == 'goals':
+                if len(splitq) == 3:
+                    key = splitq[2]
+                    if self.check_key_goals(key):
+                        self.updateGoal(key)
+                    else:
+                        self.send_404()
+            elif splitq[1] == 'users':
+                if len(splitq) == 3:
+                    key = splitq[2]
+                    if self.check_key_users(key):
+                        self.updateUser(key)
+                    else:
+                        self.send_404()
             else:
                 self.send_404()
 
@@ -114,13 +179,19 @@ class MyServer(BaseHTTPRequestHandler):
             if splitq[1] == "goals":
                 if len(splitq) == 3:
                     key = splitq[2]
-                    if self.check_key(key):
+                    if self.check_key_goals(key):
                         self.deleteGoal(key)
                     else:
                         self.send_404()
-            elif not splitq[2]: 
-                if splitq[1] == "goals":
+                elif splitq[1] == "goals":
                     self.deleteGoals()
+            elif splitq[1] == "users":
+                if len(splitq) == 3:
+                    key = splitq[2]
+                    if self.check_key_users(key):
+                        self.deleteUser(key)
+                    else:
+                        self.send_404()
             else:
                self.send_404()
 
--- sqlclass.py
+++ sqlclass.py
@@ -1,13 +1,12 @@
 import json
 import sqlite3
+from passlib.hash import bcrypt
 
 class goalsdb:
         def __init__(self):
              self.conn = sqlite3.connect("goals.db")
              self.cursor = self.conn.cursor()
         def dictToList(self, mydict):
-            print('mydict')
-            print(mydict)
             row = [
                 mydict.get('goal_name'),
                 mydict.get('goal_num'),
@@ -17,13 +16,10 @@ class goalsdb:
                 mydict.get('report_period'),
                 mydict.get('goal_per_report_period')
             ]
-            print(row)
             mylist = [item for sublist in row for item in sublist]
-            print('made list')
             return mylist
     
         def sqlCreateGoal(self, goal):
-            print(goal)
             mylist = self.dictToList(goal)
             self.cursor.execute("INSERT INTO goals VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)", (mylist))
             self.conn.commit()
@@ -46,12 +42,8 @@ class goalsdb:
             return json_goal
     
         def updateGoal(self, goal, key):
-            print(goal)
-            print(key)
             goalList = self.dictToList(goal)
-            print(goalList)
             self.cursor.execute("UPDATE goals SET goal_name = ?, goal_num = ?,goal_progress = ?, start_date = ?, end_date = ?, report_period = ?,goal_per_report_period = ? WHERE key = ?", (goalList[0] ,goalList[1],goalList[2],goalList[3],goalList[4],goalList[5],goalList[6],key))
-            print("updated goal")
             self.conn.commit()
             self.conn.close()
         
@@ -60,13 +52,80 @@ class goalsdb:
             self.conn.commit()
             self.conn.close()
         
-        def check_key(self, key):
+        def check_key_goals(self, key):
             self.cursor.execute("select exists (select key from goals where key = ?)", (key,))
             checker = self.cursor.fetchone()
             if checker[0] == 1:
                 print("true")
                 print(checker[0])
                 return True
-            
+            return False
+
+class usersdb:
+        def __init__(self):
+             self.conn = sqlite3.connect("goals.db")
+             self.cursor = self.conn.cursor()
+        def dictToList(self, mydict, hashed):
+            row = [
+                mydict.get('email'),
+                mydict.get('f_name'),
+                mydict.get('l_name'),
+                hashed
+            ]
+            mylist = [item for sublist in row for item in sublist]
+            return mylist
+
+        def hashPass(self, password):
+            return hashed = bcrypt.encrypt(password)
+
+        def sqlCreateUser(self, user):
+            hashed = hashPass(user.get('password'))
+            mylist = self.dictToList(user, hashed)
+            self.cursor.execute("INSERT INTO users VALUES (NULL, ?, ?, ?, ?)", (mylist))
+            self.conn.commit()
+            self.conn.close()
+
+        def getUsers(self):
+            self.cursor.execute("SELECT * FROM users")
+            all_users = self.cursor.fetchall()
+            json_users = json.dumps(all_users)
+            self.conn.commit()
+            self.conn.close()
+            return json_users
+        
+        def getUser(self, key):
+            self.cursor.execute("SELECT * FROM users WHERE key = ?", (key))
+            user = self.cursor.fetchone()
+            json_user = json.dumps(user)
+            self.conn.commit()
+            self.conn.close()
+            return json_user
+    
+        def updateUser(self, user, key):
+            userList = self.dictToList(user)
+            self.cursor.execute("UPDATE users SET email = ?, f_name = ?, l_name = ?, password = ?, WHERE key = ?", (userList[0] ,userList[1],userList[2],userList[3],key))
+            self.conn.commit()
+            self.conn.close()
+        
+        def deleteUser(self, key):
+            self.cursor.execute("DELETE FROM users WHERE key = ?", (key,))
+            self.conn.commit()
+            self.conn.close()
+        
+        def check_key_users(self, key):
+            self.cursor.execute("select exists (select key from users where key = ?)", (key,))
+            checker = self.cursor.fetchone()
+            if checker[0] == 1:
+                print("true")
+                print(checker[0])
+                return True
+            return False
+                et('password'),
+
+        def check_key_users(self, key):
+            self.cursor.execute("select exists (select key from users where key = ?)", (key,))
+            checker = self.cursor.fetchone()
+            if checker[0] == 1:
+                return True
             return False
 
